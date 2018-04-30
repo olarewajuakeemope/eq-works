557d0e206e14d34ce8e883773169ae9a713fe0e4
Olarewaju Opeyemi <olarewajuakeemope@icloud.com>
2018-04-30 14:35:10 +0100

    Fix reload issue with koa

--- client.js
+++ client.js
@@ -1,22 +1,25 @@
-import express from 'express'
+import Koa from 'koa'
 import webpack from 'webpack'
-import webpackDevMiddleware from 'webpack-dev-middleware'
-import webpackHotMiddleware from 'webpack-hot-middleware'
+import historyApiFallback from '@overra/koa-history-api-fallback'
+import {devMiddleware, hotMiddleware} from 'koa-webpack-middleware'
 import devConfig from './webpack.dev'
 import prodConfig from './webpack.prod'
 
-const app = express()
+const app = new Koa()
 
 const config = process.env.NODE_ENV === 'production' ? prodConfig : devConfig
 const compiler = webpack(config)
 
 // Tell express to use the webpack-dev-middleware and use the webpack.config.js
 // configuration file as a base.
-app.use(webpackDevMiddleware(compiler, {
+const webpackMiddleware = devMiddleware(compiler, {
   publicPath: config.output.publicPath,
-}));
+})
 
-app.use(webpackHotMiddleware(compiler))
+app.use(hotMiddleware(compiler))
+app.use(webpackMiddleware) // serve up webpack content
+app.use(historyApiFallback()) // catch any other requests and redirect to /index.html
+app.use(webpackMiddleware)
 
 // Serve the files on port 3000.
 app.listen(process.env.PORT || 3000)
